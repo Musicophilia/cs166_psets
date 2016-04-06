c4b21896dec1c884a30139ebb515ca2f2a6e4efe
Lisa Wang <lisa1010@stanford.edu>
2016-04-06 02:29:26 -0700

    Fixed HybridRMQ to use no preprocessing on bottom structure.

--- ps1/rmq/HybridRMQ.java
+++ ps1/rmq/HybridRMQ.java
@@ -1,8 +1,5 @@
 package rmq;
-import java.util.Arrays;
-import java.util.Collections;
 
-// import SparseTableRMQ.java;
 /**
  * An &lt;O(n), O(log n)&gt; implementation of the RMQ as a hybrid between
  * the sparse table (on top) and no-precomputation structure (on bottom)
@@ -17,12 +14,16 @@ public class HybridRMQ implements RMQ {
      * @elems The array over which RMQ should be computed.
      */
 
+	// Instance Variables
     private SparseTableRMQ topRMQ;
-    private SparseTableRMQ bottomRMQ;
-    private float[] elements;
+    private float[] elements; // to store a copy of the elements
     private int n = 0; // number of elements being passed into this RMQ
     private int b = 0; // block size
     
+    /**
+     * Computes the minima for each block and returns the top level
+     * elements (block minima) as an array of floats.
+     */
     private float[] computeBlockMins() {
     	int numTopElems = (int) n/b;
     	if (n%b != 0) numTopElems++; // if n is not multiple of b, there is one more block
@@ -44,11 +45,26 @@ public class HybridRMQ implements RMQ {
     public HybridRMQ(float[] elems) {
         elements = elems.clone();
         n = elems.length;
-        b = (int) (Math.log(n)/Math.log(2)) + 1; // compute blocksize, natural log
-        
+        b = (int) (Math.log(n)/Math.log(2)) + 1; // compute blocksize using log base 2, add 1 to avoid b = 0
         float[] topElems = computeBlockMins();
         topRMQ = new SparseTableRMQ(topElems);
-        bottomRMQ = new SparseTableRMQ(elems);
+    }
+    
+    /**
+     * Evaluates RMQ(i, j) over the array stored by the constructor, returning
+     * the index of the minimum value in that range, by using a linear search.
+     * No pre-computation, used for bottom structure.
+     */
+    private int findMinIndex(int i, int j) {
+    	float minValue = Float.MAX_VALUE;
+        int minIndex = 0;
+    	for (int k = i; k <= j; k++) {
+            if (elements[k] < minValue) {
+        		minValue = elements[k];
+        		minIndex = k;
+        	}
+    	}
+    	return minIndex;
     }
 
     /**
@@ -58,7 +74,7 @@ public class HybridRMQ implements RMQ {
     @Override
     public int rmq(int i, int j) {
 //    	System.out.println("n=" + Integer.toString(n) + "\tb=" + Integer.toString(b) + "\ti=" + Integer.toString(i) +  "\tj=" + Integer.toString(j));
-    	if (n == 0) return bottomRMQ.rmq(i, j);
+    	if (n == 0) return 0;
     	
     	int iTop = i/b + 1;
     	int jTop = j/b - 1;
@@ -74,9 +90,9 @@ public class HybridRMQ implements RMQ {
         	if (iRight > jRight) iRight = jRight;
         	
         	int topMinIndexRaw = topRMQ.rmq(iTop, jTop) ;
-        	int topMinIndex = bottomRMQ.rmq(topMinIndexRaw * b, topMinIndexRaw * b + b - 1);
-            int leftMinIndex = bottomRMQ.rmq(iLeft, jLeft);
-            int rightMinIndex = bottomRMQ.rmq(iRight, jRight);
+        	int topMinIndex = findMinIndex(topMinIndexRaw * b, topMinIndexRaw * b + b - 1);
+            int leftMinIndex = findMinIndex(iLeft, jLeft);
+            int rightMinIndex = findMinIndex(iRight, jRight);
 
         	int[] allMinIndices = {topMinIndex, leftMinIndex, rightMinIndex};
             
@@ -93,7 +109,7 @@ public class HybridRMQ implements RMQ {
             return minIndex;
         }
         else {
-        	return bottomRMQ.rmq(i, j);
+        	return findMinIndex(i, j);
         }
     }
 }
